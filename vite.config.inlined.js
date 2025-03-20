69fc310a613f377af66b5544a1f0b28855b8e81f
Y.Ukrainets <yuriy.ukrainets@gmail.com>
2025-03-21 00:26:15 +0200

    Add build

--- vite.config.inlined.js
+++ vite.config.inlined.js
@@ -17,6 +17,10 @@ export default defineConfig({
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
+  build: {
+    outDir: './docs',
+    emptyOutDir: true,
+  },
   server: {
     open: true,
   },
